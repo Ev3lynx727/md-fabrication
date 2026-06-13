c9b5909dd8a69719f67008ea4dd0add5c98a5159
Ev3lynx727 <evelyn.ofdrive1@gmail.com>
2026-06-13 08:02:16 +0700

    fix: use @ts-ignore instead of @ts-expect-error for ESM/CJS compat

--- src/core/resolve-root.ts
+++ src/core/resolve-root.ts
@@ -1,7 +1,8 @@
 import * as path from 'path'
 import { fileURLToPath } from 'url'
 
-// @ts-expect-error - import.meta.url only valid in ESM; suppressed for CJS build
+/* eslint-disable-next-line @typescript-eslint/ban-ts-comment */
+// @ts-ignore - import.meta.url only valid in ESM; suppressed for CJS build
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 export const PROJECT_ROOT = path.resolve(dirname, '..', '..')
